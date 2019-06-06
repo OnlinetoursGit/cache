630cd340da4977e00c48c6bcbddc60193609485a
Michael Klimenko <michaelkl@mail.ru>
2019-06-06 23:01:20 +0500

    Fix key conflict warning by converting them into methods

--- spec/exts/fields_spec.rb
+++ spec/exts/fields_spec.rb
@@ -17,18 +17,42 @@ end
 describe TestFieldsCache do
 
   describe "#fetch" do
+    let(:key) { double("key") }
+    let(:object) { double("object", field1: 'aaa', field2: 123) }
 
-    it 'should load missed object as mash with given fields' do
-      key = double("key")
-      object = double("object", field1: 'aaa', field2: 123)
+    context 'present? and blank? methods are not defined in Hash' do
+      it 'should load missed object as mash with given fields' do
+        expect(subject.cache_store).to receive(:read).and_return(nil)
+        expect(subject).to receive(:load_object).with(key).and_return(object)
+        expect(subject.cache_store).to receive(:write)
 
-      expect(subject.cache_store).to receive(:read).and_return(nil)
-      expect(subject).to receive(:load_object).with(key).and_return(object)
-      expect(subject.cache_store).to receive(:write)
-
-      expect(subject.fetch(key)).to eq ::Hashie::Mash.new(field1: 'aaa', field2: 123, blank?: false, present?: true)
+        value = subject.fetch(key)
+        expect(value.field1).to eq 'aaa'
+        expect(value.field2).to eq 123
+        expect(value.blank?).to eq false
+        expect(value.present?).to eq true
+      end
     end
 
+    context 'present? and blank? methods already exist in Hash' do
+      before do
+        Hash.define_method(:blank?) { false }
+        Hash.define_method(:present?) { true }
+      end
+
+      it 'should not add blank? and present? keys unless missed' do
+        expect(subject.cache_store).to receive(:read).and_return(nil)
+        expect(subject).to receive(:load_object).with(key).and_return(object)
+        expect(subject.cache_store).to receive(:write)
+
+
+        value = subject.fetch(key)
+        expect(value.field1).to eq 'aaa'
+        expect(value.field2).to eq 123
+        expect(value.blank?).to eq false
+        expect(value.present?).to eq true
+      end
+    end
   end
 
 end
